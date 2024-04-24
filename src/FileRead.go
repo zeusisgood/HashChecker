fea5ee4c4c6a29aedd74301f0be8ce703cb6c3f2
zeusisgood <hayakawayuuto.com@gmail.com>
2024-04-25 01:49:24 +0900

    Wip:2024_04_25

--- src/FileRead.go
+++ src/FileRead.go
@@ -9,18 +9,17 @@ import (
 	"github.com/sqweek/dialog"
 )
 
-func FileRead() {
-	filename, err := dialog.File().Load()
+func FileRead() (readfile string, err error) {
+	targetfile, err := dialog.File().Load()
 	if err != nil {
-		log.Fatal("File read Error", err)
-		return
+		log.Fatal("File Read Error", err)
 	}
-	file, err := os.Open(filename)
+	file, err := os.Open(targetfile)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("File Open Error", err)
 	}
-	fmt.Println(file)
 	defer file.Close()
 	bf := bufio.NewReader(file)
-
+	fmt.Println(bf)
+	return bf, nil
 }
---
+++ src/SHA256.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func sha256() {
+	fmt.Println("empty")
+}
