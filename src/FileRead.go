86c4c523e77790ff78586d4a37709c0a297eb67d
zeusisgood <hayakawayuuto.com@gmail.com>
2024-04-30 05:14:22 +0900

    Fix: io.Copyを採用しメモリ使用量低減

--- src/FileRead.go
+++ src/FileRead.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"io"
 	"log"
-	"os"
 
 	"github.com/sqweek/dialog"
 )
 
-func FileRead() (readfile []byte) {
-	targetfile, err := dialog.File().Load()
+func FileRead() (readfile string) {
+	selectedFile, err := dialog.File().Load()
 	if err != nil {
 		log.Fatal("File Read Error", err)
 	}
-	file, err := os.Open(targetfile)
-	if err != nil {
-		log.Fatal("File Open Error", err)
-	}
-
-	defer file.Close()
-
-	readfile, err = io.ReadAll(file)
-	if err != nil {
-		log.Fatal("File Read Error", err)
-	}
-	return
+	return selectedFile
 }
