9e09b4bfa993a7082d4bc1d176a9ab1c4bd4ba0e
zeusisgood <hayakawayuuto.com@gmail.com>
2024-05-02 03:55:08 +0900

    Wip

--- src/FileRead.go
+++ src/FileRead.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/sqweek/dialog"
 )
 
-func FileRead() (readfile string) {
-	selectedFile, err := dialog.File().Load()
-	if err != nil {
-		log.Fatal("File Read Error", err)
+func FileRead() (selectedFileSlice []string) {
+	for {
+		fmt.Println("ファイルを一つずつ選択してください")
+		temp, err := dialog.File().Load()
+		if err != nil {
+			fmt.Println("ファイル選択end")
+			break
+		}
+		selectedFileSlice = append(selectedFileSlice, temp)
+		fmt.Println(temp)
 	}
-	return selectedFile
+	return
 }
--- src/SHA256.go
+++ src/SHA256.go
@@ -9,19 +9,21 @@ import (
 	"os"
 )
 
-func hash_sha256(readfile string) {
-	file, err := os.Open(readfile)
-	if err != nil {
-		log.Fatal("File Open Error", err)
+func hash_sha256(readfile []string) {
+	for i, v := range readfile { // TODO for内の処理追記
+		file, err := os.Open(v)
+		if err != nil {
+			log.Fatal("File Open Error", err)
+		}
+		defer file.Close()
+		hash := sha256.New()
+		if _, err := io.Copy(hash, file); err != nil {
+			log.Fatal(err)
+		}
+		hashInBytes := hash.Sum(nil)
+		temp := hex.EncodeToString(hashInBytes)
+		fmt.Println(v)
+		fmt.Println(i+1, "sha256")
+		fmt.Println(temp)
 	}
-
-	defer file.Close()
-
-	hash := sha256.New()
-	if _, err := io.Copy(hash, file); err != nil {
-		log.Fatal(err)
-	}
-
-	hashInBytes := hash.Sum(nil)
-	fmt.Println("sha256", hex.EncodeToString(hashInBytes))
 }
